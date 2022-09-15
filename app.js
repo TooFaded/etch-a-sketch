8b0b3ec06e5b8762d9eec99302557391c7d26731
toofaded <melvin_torres17@live.com>
2022-09-15 16:56:04 -0400

    Added buttons for color options, added an input element to allow user to insert a value that can change grid size, added event listener so the mouseover will change div background color to black

--- app.js
+++ app.js
@@ -1,20 +1,37 @@
-//Consts and vars
-const container = document.getElementById("container")
-let rows = document.getElementsByClassName("gridRow")
-let cells = document.getElementsByClassName("gridCol")
-container.style.gridTemplateColumns = "repeat(16, 1fr)"
-container.style.gridTemplateRows = "repeat(16, 1fr)"
-
-function createDivs(col, rows) {
+//Creates board
+function createDivs(size) {
     
-    for(i = 0; i < (col * rows); i++){
+    const container = document.getElementById("container")
+    container.style.gridTemplateColumns = `repeat(${size}, 1fr)`
+    container.style.gridTemplateRows = `repeat(${size}, 1fr)`
+    let boxes = container.querySelectorAll("div");
+    boxes.forEach((div) => div.remove());
+    
+    
+    let amount = size *size;
+    for(i = 0; i < amount; i++){
        const div = document.createElement("div")
        container.appendChild(div).classList.add("box")
-      
+       div.addEventListener("mouseover", function(e){
+        this.style.backgroundColor = "black";
+    });
+      container.insertAdjacentElement("beforeend", div);
     }
 }
 
-createDivs(16,16);
+function changeSize(input){
+    
+    if(input >= 2 && input <= 100){
+        createDivs(input);
+    } else {
+        console.log("Invalid value!")
+    }
+    
+
+
+}
+
+createDivs(16);
 
 
 
