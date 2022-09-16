7d283a2b619e0ff4d481e0b5519e3da08ea17d20
toofaded <melvin_torres17@live.com>
2022-09-15 21:24:45 -0400

    finished the core logic for etch a sketch, created a function to change the value of color so the  user can change color of each div, changed size of input box and included a span to show the grids dimensions as its being changed, decorated buttons and added hover effects and shadowing, added an icon to the eraser button and created a function to reset the etch a sketch canvas, added a font for the title heading and decorated heading.

--- app.js
+++ app.js
@@ -12,22 +12,40 @@ function createDivs(size) {
     for(i = 0; i < amount; i++){
        const div = document.createElement("div")
        container.appendChild(div).classList.add("box")
-       div.addEventListener("mouseover", function(e){
-        this.style.backgroundColor = "black";
+       div.addEventListener("mouseover", function(e) {
+        this.style.backgroundColor = color;
     });
       container.insertAdjacentElement("beforeend", div);
     }
 }
 
+//  Changes color of pen
+let color = "black"
+function changeColor(choice) {
+    color = choice;
+
+
+
+}
+// Resizes the grid depending on input
 function changeSize(input){
     
     if(input >= 2 && input <= 100){
         createDivs(input);
+        let span = document.getElementById("currentSize")
+        span.innerHTML = `${input} X`
     } else {
         console.log("Invalid value!")
     }
     
 
+
+}
+//Replaces the colored divs with white ones
+function resetCanvas() {
+    let container = document.getElementById("container")
+    let boxes = container.querySelectorAll("div");
+    boxes.forEach((div) => div.style.backgroundColor = "white");
 
 }
 
